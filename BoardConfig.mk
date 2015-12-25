d1421d1a84508872ed3e771e6fb6a8dec1693719
jollaman999 <admin@jollaman999.com>
2015-12-26 00:23:50 +0900

    TWRP build support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,4 +165,18 @@ TARGET_PER_MGR_ENABLED := true
 BOARD_USES_CYANOGEN_HARDWARE := true
 BOARD_HARDWARE_CLASS := hardware/cyanogen/cmhw
 
+# TWRP
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INCLUDE_CRYPTO := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+
+PRODUCT_COPY_FILES += \
+    device/lge/bullhead/twrp.fstab:recovery/root/etc/twrp.fstab
+
 -include vendor/lge/bullhead/BoardConfigVendor.mk
