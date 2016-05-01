7a7bf212342a9d20f4b997638ccc4ecc6243cf66
Joshndroid <fmxjoshiewah@gmail.com>
2016-05-01 17:06:44 +1000

    Gello : Add gello browser
    Change-Id: Icaa4fb9aa8d146c219a2b636314596a755d54d72
    (cherry picked from commit a5ac2b07bfff2d56629473d30ab15ecc18178b49)

--- device.mk
+++ device.mk
@@ -194,6 +194,10 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     rmnetcli
 
+# Browser
+PRODUCT_PACKAGES += \
+    Gello
+
 # GPS configuration
 PRODUCT_COPY_FILES += \
     device/lge/bullhead/gps.conf:system/etc/gps.conf
